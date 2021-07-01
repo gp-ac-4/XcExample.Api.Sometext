dcd0d39f1d45f2446e6c218d89ccac858e0d6d58
AC4 <github@nboot.net>
2021-07-01 14:12:22 -0600

    adding controllers and docker file

---
+++ XcExample.Api.Sometext/Controllers/SomeTextController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace XcExample.Api.Sometext.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SomeTextController : ControllerBase
+    {
+
+        private readonly ILogger<SomeTextController> _logger;
+
+        public SomeTextController(ILogger<SomeTextController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+            return new List<string>()
+            {
+                "Yup, a string"
+            }
+            .ToArray();
+        }
+    }
+}
---
+++ XcExample.Api.Sometext/Controllers/TestHeadersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace XcExample.Api.Sometext.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestHeadersController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var headers = new List<string>();
+            foreach (var headerPair in Request.Headers)
+            {
+                headers.Add($"{headerPair.Key}:{headerPair.Value}");
+            }
+            return headers.ToArray();
+        }
+    }
+}
