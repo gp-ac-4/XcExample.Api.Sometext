e7af296028f8b7d129ae56b0b8678270db37a26a
AC4 <github@nboot.net>
2021-07-04 11:13:42 -0600

    adding tye and publish info

--- XcExample.Api.Sometext/Startup.cs
+++ XcExample.Api.Sometext/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -43,6 +44,7 @@ namespace XcExample.Api.Sometext
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("/", async context => await context.Response.WriteAsync("ALIVE!"));
                 endpoints.MapControllers();
             });
         }
